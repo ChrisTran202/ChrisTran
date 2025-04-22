1fff20c3b8f705224d42f1f259ec76633dc03c32
Chris Tran <christran202@gmail.com>
2025-04-21 22:12:05 -0500

    update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,10 @@ function Intro()  {
 
   const rightArrow = '>'
 
+  
+
   return (
+
     
     <section className='flex flex-col gap-5 items-center h-[80vh] md:h-[85vh] justify-center mt-[5rem] animate-fade-up animate-duration-1000 animate-delay-[200ms] animate-ease-in-out animate-fill-both '>
       <div className='flex bg-[url(./assets/Skyscape.jpg)]  bg-[#21083e]  sm:bg-auto md:bg-cover  bg-no-repeat bg-center rounded-t-[25rem] flex-col items-center w-[80%] h-[80vh] md:h-[90vh] rounded-[5rem]  border-[#ff9d48] border-[.1rem] '>
@@ -40,10 +43,11 @@ function Intro()  {
               {/* linkedin */}
               <a 
                 href='https://www.linkedin.com/in/christopherctran/' 
+                
                 target='_blank' 
                 className='flex justify-center p-6 animate-fade-left animate-duration-700 animate-delay-[1100ms] animate-ease-in animate-fill-both '
               >
-                <FaLinkedin className='bg-[#ffa600] border-[#da28fd] hover:border-white hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] border-[.2rem] rounded-md  w-[2rem] h-[2rem]  md:h-[3rem] md:w-[3rem] hover:scale-125 ease-in-out duration-200 '/>
+                <FaLinkedin className='bg-[#ffa600] border-[#da28fd] hover:border-white hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] border-[.2rem] rounded-md  w-[2rem] h-[2rem]  md:h-[3rem] md:w-[3rem] hover:scale-125 ease-in-out duration-200 animate-bounce  animate-duration-1000 '/>
               </a>
               
               {/* email */}
@@ -57,8 +61,9 @@ function Intro()  {
             </div>
 
             <div className='flex flex-row items-center justify-center animate-fade-left animate-duration-700 animate-delay-[1000ms] animate-ease-in animate-fill-both'>
+             
               {/* resume button */}
-                <button className='flex items-center justify-center hover:scale-125 ease-in-out duration-200   bg-[#ffa600] border-[#da28fd] hover:border-white  hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] border-[.2rem] rounded-xl w-[7rem] h-[2rem] md:w-[8.5rem] md:h-[3rem] animate-bounce  animate-duration-1000'>
+                {/* <button className='flex items-center justify-center hover:scale-125 ease-in-out duration-200   bg-[#ffa600] border-[#da28fd] hover:border-white  hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] border-[.2rem] rounded-xl w-[7rem] h-[2rem] md:w-[8.5rem] md:h-[3rem] animate-bounce  animate-duration-1000'>
                   <a 
                   href='' 
                   target='_blank' 
@@ -68,13 +73,15 @@ function Intro()  {
                         Resume
                       </p>
                   </a>
-                </button>
+                </button> */}
+
             </div>
           </div>
 
           </div>
         </div>
         
+
       
       {/* right arrow */}
       <div className='flex flex-row items-center justify-end w-[80%]'>
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-
+import page1 from '../../assets/rimz-page-final.png'
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -9,19 +9,21 @@ import 'swiper/css/pagination';
 
 const projects = [
   {
-    title: 'Project 1',
-    description: 'A brief description of Project 1.',
-    image: 'https://via.placeholder.com/300x200',
+    title: '',
+    description: '.',
+    image: '',
   },
   {
-    title: 'Rim',
-    description: 'A brief description of Project 2.',
-    image: 'https://via.placeholder.com/300x200',
+    title: 'RimZ',
+    description: 'I really love the car scence, this was create for my car enthusiast people. Created with Vite, Redux, Tailwind CSS.',
+    image: page1,
+    link: 'https://rimz.vercel.app/'
   },
   {
-    title: 'Project 3',
-    description: 'A brief description of Project 3.',
-    image: 'https://via.placeholder.com/300x200',
+    title: '',
+    description: '',
+    image: '',
+    
   },
 ];
 
@@ -30,37 +32,48 @@ const Carousel = () => {
     <div className='flex items-center justify-center h-[80%] m-4 '>
       
       <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={60}
+        modules={[Navigation, Pagination, Autoplay ]}
+        spaceBetween={30}
         slidesPerView={1}
-        navigation
+        navigation={{ clickable: true }}
         pagination={{ clickable: true }}
         autoplay={{ delay: 4000 }}
+        hashNavigation={true}
         breakpoints={{
           640: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
+          700: { slidesPerView: 2 },
+          1500: { slidesPerView: 3 },
         }}
         
       >
+        
         {projects.map((project, index) => (
           <SwiperSlide key={index}
             
           >
+            <a href={projects.link} target='_blank'>
             <div className='bg-white shadow-[#ffa600] shadow-lg rounded-lg overflow-hidden bg-clip-text text-transparent hover:scale-350 ease-in-out duration-400 hover:shadow-xl'>
+              
               <img
                 src={project.image}
                 alt={project.title}
-                className='w-full h-auto object-cover'
+                className='w-full md:[20rem] h-auto object-cover'
               />
-              <div className='flex justify-center align-center p-4'>
-                <h3 className='text-xl font-semibold my-[10vh] mr-10'>{project.title}</h3>
+              <div className='flex flex-col justify-center text-center align-center p-4'>
+                <h3 className='text-xl font-semibold my-[6vh] my:[10vh] mr-10'>{project.title}</h3>
                 <p className='mt-2 text-white-600'>{project.description}</p>
+                <ref href={projects.link} target='_blank' className='text-[#ff9d48] hover:text-[#ff9d48] hover:scale-125 ease-in-out duration-200 cursor-pointer'>
+                  Live Site
+                  
+                </ref>
               </div>
-            </div>
+              
+            </div> 
+            </a>
           </SwiperSlide>
         ))}
       </Swiper>
+      
     </div>
   );
 };
